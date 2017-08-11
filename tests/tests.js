18e9c2ff415db316420e8546f4f77b0cb9a0b9e6
maverickmishra <vishalmishra0907@gmail.com>
2017-08-10 18:28:44 -0700

    :white_check_mark: Added basic tests

--- tests/tests.js
+++ tests/tests.js
@@ -19,6 +19,76 @@
  *
 */
 
-/* globals ImageCapture */
+/* globals MediaRecorder */
 /* jshint jasmine: true */
+
+exports.defineAutoTests = function() {
+
+	describe('phonegap-plugin-media-recorder', function () {
+
+		describe('MediaRecorder', function () {
+
+        	it('should exist', function () {
+            	expect(MediaRecorder).toBeDefined();
+            	expect(typeof MediaRecorder === 'function').toBe(true);
+        	});
+
+        	it('should take a MediaStream as constructor arguement', function() {
+            	var p = navigator.mediaDevices.getUserMedia({
+         					audio: true,
+         					video: true
+     					});
+            	p.then(function(stream){
+            		var media = new MediaRecorder(stream);
+            		expect(media).toBeDefined();
+            		expect(typeof media === 'object').toBe(true);
+            		expect(media.stream).toBeDefined();
+            		expect(media.stream.id).toBeDefined();
+            		expect(typeof media.stream.id).toBe('string');
+            		expect(media.stream.getVideoTracks()).toBeDefined();
+                    expect(media.stream.getAudioTracks()).toBeDefined();
+                    expect(media.stream.getTracks()).toBeDefined();
+                    expect(media.stream.getTrackbyId()).toBeDefined();
+                    expect(media.stream.addTrack()).toBeDefined();
+                    expect(media.stream.removeTrack()).toBeDefined();
+                    expect(media.stream.onaddtrack).toBeDefined();
+                    expect(media.stream.onremovetrack).toBeDefined();
+                    expect(media.stream.clone()).toBeDefined();
+                    expect(media.stream.active).toBeDefined();
+                    expect(typeof media.stream.active).toBe('boolean');
+            		expect(media.mimeType).toBeDefined();
+            		expect(typeof media.mimeType).toBe('string');
+            		expect(media.state).toBeDefined();
+            		var recState = ["inactive", "recording","paused"];
+            		expect(recState).toContain(media.state);
+            		expect(media.onstart).toBeDefined();
+            		expect(media.onstop).toBeDefined();
+            		expect(media.ondataavailable).toBeDefined();
+            		expect(media.onpause).toBeDefined();
+            		expect(media.onresume).toBeDefined();
+            		expect(media.onerror).toBeDefined();
+            		expect(media.videoBitsPerSecond).toBeDefined();
+            		expect(typeof media.videoBitsPerSecond).toBe('number');
+            		expect(media.audioBitsPerSecond).toBeDefined();
+            		expect(typeof media.audioBitsPerSecond).toBe('number');
+            		expect(media.start).toBeDefined();
+            		expect(typeof media.start).toBe('function');
+            		expect(media.stop).toBeDefined();
+            		expect(typeof media.stop).toBe('function');
+            		expect(media.pause).toBeDefined();
+            		expect(typeof media.pause).toBe('function');
+            		expect(media.resume).toBeDefined();
+            		expect(typeof media.resume).toBe('function');
+            		expect(media.requestData).toBeDefined();
+            		expect(typeof media.requestData).toBe('function');
+            		expect(media.isTypeSupported).toBeDefined();
+
+
+            	})
+        	});
+
+    	});
+    
+    });
+};
 
