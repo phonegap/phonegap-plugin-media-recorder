a8871b4124c9e7db3f2ad02466a193ef95e80ba4
maverickmishra <vishalmishra0907@gmail.com>
2017-08-14 18:22:19 -0700

    :apple: Started JS implementation

--- www/MediaRecorder.js
+++ www/MediaRecorder.js
@@ -22,8 +22,99 @@
 var exec = cordova.require('cordova/exec');
 var utils = cordova.require('cordova/utils');
 
-var MediaRecorder = function (stream) {
+var MediaRecorder = function (stream, options) {
+    this.stream = stream;
+    if(!options.mimeType){
+    	this.mimeType = '';
+    };
+    this.state = 'inactive';
+    if(!options.videoBitsPerSecond){
+    	this.videoBitsPerSecond = 0;
+    };
+    if(!options.audioBitsPerSecond){
+    	this.audioBitsPerSecond = 0;
+    };
+    this.onstart = function () {};
+    this.onstop = function () {};
+    this.ondataavailable = function () {};
+    this.onpause = function () {};
+    this.onresume = function () {};
+    this.onerror = function () {};
+};
 
+MediaRecorder.prototype.start = function (timeslice) {
+    if(this.state != 'inactive'){
+        var name = 'InvalidStateError';
+        var description = '';
+        var domException = new DOMException(description, name);
+        return domException;
+    }
+    else {
+    	this.state = 'recording';
+        var success = function (info) {           
+            this.onstart();       
+        };
+        var fail = function (error) {      
+        };
+        exec(success, fail, 'MediaRecorder', 'start', [timeslice]);
+    }
+};
+MediaRecorder.prototype.stop = function () {
+    if(this.state === 'inactive'){
+        var name = 'InvalidStateError';
+        var description = '';
+        var domException = new DOMException(description, name);
+        return domException;
+    }
+    else {
+    	this.state = 'inactive';
+        var success = function (info) {           
+            this.onstop();       
+        };
+        var fail = function (error) {      
+        };
+        exec(success, fail, 'MediaRecorder', 'stop', []);
+    }
+};
+MediaRecorder.prototype.pause = function () {
+    if(this.state === 'inactive'){
+        var name = 'InvalidStateError';
+        var description = '';
+        var domException = new DOMException(description, name);
+        return domException;
+    }
+    else {
+    	this.state = 'paused';
+        var success = function (info) {           
+            this.onpause();       
+        };
+        var fail = function (error) {      
+        };
+        exec(success, fail, 'MediaRecorder', 'pause', []);
+    }
+};
+MediaRecorder.prototype.resume = function () {
+	if(this.state === 'inactive'){
+        var name = 'InvalidStateError';
+        var description = '';
+        var domException = new DOMException(description, name);
+        return domException;
+    }
+    else {
+    	this.state = 'recording';
+        var success = function (info) {           
+            this.onresume();       
+        };
+        var fail = function (error) {      
+        };
+        exec(success, fail, 'MediaRecorder', 'resume', []);
+    }
+};
+MediaRecorder.prototype.requestData = function () {
+    this.ondataavailable();
+};
+MediaRecorder.prototype.isTypeSupported = function (type) {
+   // return true/false;
 };
 
 module.exports = MediaRecorder;
