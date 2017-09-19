b389a46e9fc97599982569911943cfc8c0253b95
maverickmishra <vishalmishra0907@gmail.com>
2017-09-19 10:22:51 -0700

    :white_check_mark: Issue #12. Added more spec tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -5,6 +5,16 @@
  */
 var cordova = require('./helper/cordova');
 var MediaRecorder = require('../www/MediaRecorder');
+// audio only
+var stream1 = {id: "8EF37333-A700-4082-AE9A-78211403DD4C", audioTracks: [{id: "7B0A51E2-F6F1-45E0-A743-BDD8A1B7D33D", kind: "audio", description: "AVCaptureDeviceTypeBuiltInMicrophone"}], videoTracks: [], getAudioTracks: function() { return stream1.audioTracks;}, getVideoTracks: function() { return stream1.videoTracks;}};
+// audio + video
+var stream2 = {id: "E13D8A74-A1AC-40E3-A3A6-C78262CDC5B7", audioTracks: [{id: "899B406A-B1B7-42EC-AE33-283001D203EF", kind: "audio", description: "AVCaptureDeviceTypeBuiltInMicrophone"}], videoTracks: [{id: "C8F600ED-6C3D-41A7-AAEC-0294A8FBDAD5", kind: "video", description: "rearcamera"}, {id: "77F46117-0971-4356-AF12-E3BE64070CC6", kind: "video", description: "frontcamera"}], getAudioTracks: function() { return stream2.audioTracks;}, getVideoTracks: function() { return stream2.videoTracks;}};
+// video only
+var stream3 = {id: "D856DDA8-D3C0-46A7-88B7-ECD8A8DE9A29", audioTracks: [], videoTracks: [{id: "67761BF6-F1F1-496D-B8CE-FA0394AEDFDE", kind: "video", description: "rearcamera"}, {id: "0E9146B0-CF9B-4CDE-8A0C-5F2C76EBEDD6", kind: "video", description: "frontcamera"}], getAudioTracks: function() { return stream3.audioTracks;}, getVideoTracks: function() { return stream3.videoTracks;}};
+// none
+var stream4 = {id: "8F825B0A-7894-46F8-8C3D-F4E4458BE29E", audioTracks: [], videoTracks: [], getAudioTracks: function() { return stream4.audioTracks;}, getVideoTracks: function() { return stream4.videoTracks;}};
+
+
 
 describe('phonegap-plugin-media-recorder', function () {
 
@@ -15,71 +25,145 @@ describe('phonegap-plugin-media-recorder', function () {
             expect(typeof MediaRecorder === 'function').toBe(true);
         });
 
-        it('should take a MediaStream as constructor argument', function (done) {
-            try {
-                var constraints = {
-                    audio: true,
-                    video: true
-                };
-                var p = navigator.mediaDevices.getUserMedia(constraints);
-                expect(p).toBeDefined();
-                p.then(function (stream) {
-                    var media = new MediaRecorder(stream);
-                    expect(media).toBeDefined();
-                    expect(typeof media === 'object').toBe(true);
-                    expect(media.stream).toBeDefined();
-                    expect(media.stream.id).toBeDefined();
-                    expect(typeof media.stream.id).toBe('string');
-                    expect(media.stream.getVideoTracks()[0]).toBeDefined();
-                    console.log(media.stream.getVideoTracks[0]);
-                    console.log(typeof media.stream.getVideoTracks[0]);
-                    expect(media.stream.getAudioTracks()[0]).toBeDefined();
-                    expect(media.stream.getTracks()).toBeDefined();
-                    expect(media.stream.getTrackbyId()).toBeDefined();
-                    expect(media.stream.addTrack()).toBeDefined();
-                    expect(media.stream.removeTrack()).toBeDefined();
-                    expect(media.stream.onaddtrack).toBeDefined();
-                    expect(media.stream.onremovetrack).toBeDefined();
-                    expect(media.stream.clone()).toBeDefined();
-                    expect(media.stream.active).toBeDefined();
-                    expect(typeof media.stream.active).toBe('number');
-                    expect(media.mimeType).toBeDefined();
-                    expect(typeof media.mimeType).toBe('string');
-                    expect(media.state).toBeDefined();
-                    var recState = ['inactive', 'recording', 'paused'];
-                    expect(recState).toContain(media.state);
-                    expect(media.onstart).toBeDefined();
-                    expect(media.onstop).toBeDefined();
-                    expect(media.ondataavailable).toBeDefined();
-                    expect(media.onpause).toBeDefined();
-                    expect(media.onresume).toBeDefined();
-                    expect(media.onerror).toBeDefined();
-                    expect(media.videoBitsPerSecond).toBeDefined();
-                    expect(typeof media.videoBitsPerSecond).toBe('number');
-                    expect(media.audioBitsPerSecond).toBeDefined();
-                    expect(typeof media.audioBitsPerSecond).toBe('number');
-                    expect(media.start).toBeDefined();
-                    expect(typeof media.start).toBe('function');
-                    expect(media.stop).toBeDefined();
-                    expect(typeof media.stop).toBe('function');
-                    expect(media.pause).toBeDefined();
-                    expect(typeof media.pause).toBe('function');
-                    expect(media.resume).toBeDefined();
-                    expect(typeof media.resume).toBe('function');
-                    expect(media.requestData).toBeDefined();
-                    expect(typeof media.requestData).toBe('function');
-                    expect(media.isTypeSupported).toBeDefined();
-                    expect(typeof media.isTypeSupported).toBe('function');
-                    done();
-                }, function (err) {
-                    expect(err).toBeDefined();
-                    console.log(err);
-                    done();
-                });
-            } catch (err) {
-                done();
-            }
+        it('should take a MediaStream as constructor argument with audio', function () {
+            var options = {mimeType : 'video/quicktime'};
+            var media = new MediaRecorder(stream1);
+            expect(media).toBeDefined();
+            expect(typeof media === 'object').toBe(true);
+            expect(media.stream).toBeDefined();
+            expect(media.mimeType).toBeDefined();
+            expect(typeof media.mimeType).toBe('string');
+            expect(media.state).toBeDefined();
+            var recState = ['inactive', 'recording', 'paused'];
+            expect(recState).toContain(media.state);
+            expect(media.onstart).toBeDefined();
+            expect(media.onstop).toBeDefined();
+            expect(media.ondataavailable).toBeDefined();
+            expect(media.onpause).toBeDefined();
+            expect(media.onresume).toBeDefined();
+            expect(media.onerror).toBeDefined();
+            expect(media.videoBitsPerSecond).toBeDefined();
+            expect(typeof media.videoBitsPerSecond).toBe('number');
+            expect(media.audioBitsPerSecond).toBeDefined();
+            expect(typeof media.audioBitsPerSecond).toBe('number');
+            expect(media.start).toBeDefined();
+            expect(typeof media.start).toBe('function');
+            expect(media.stop).toBeDefined();
+            expect(typeof media.stop).toBe('function');
+            expect(media.pause).toBeDefined();
+            expect(typeof media.pause).toBe('function');
+            expect(media.resume).toBeDefined();
+            expect(typeof media.resume).toBe('function');
+            expect(media.requestData).toBeDefined();
+            expect(typeof media.requestData).toBe('function');
+            expect(media.isTypeSupported).toBeDefined();
+            expect(typeof media.isTypeSupported).toBe('function'); 
+            expect(media.isTypeSupported('video/mp4')).toEqual(false);
+            expect(media.isTypeSupported('audio/m4a')).toEqual(true);
+            expect(media.isTypeSupported('audio/wav')).toEqual(true);
+            expect(media.isTypeSupported('video/quicktime')).toEqual(true);
+            expect(media.isTypeSupported('')).toEqual(true);
+            expect(media.state).toBe('inactive');
+            // spyOn(media,'onstart');
+            media.start();
+            expect(media.state).toBe('recording');
+            // expect(media.onstart).toHaveBeenCalled();
+            media.pause();
+            expect(media.state).toBe('paused');
+            media.resume();
+            expect(media.state).toBe('recording');
+            // spyOn(media,'onstop');
+            media.stop();
+            expect(media.state).toBe('inactive');
+             
         });
 
+        it('should take a MediaStream as constructor argument with audio + video', function () {
+            var options = {mimeType : 'video/quicktime'};
+            var media = new MediaRecorder(stream2, options);
+            expect(media).toBeDefined();
+            expect(typeof media === 'object').toBe(true);
+            expect(media.stream).toBeDefined();
+            expect(media.mimeType).toBeDefined();
+            expect(typeof media.mimeType).toBe('string');
+            expect(media.state).toBeDefined();
+            var recState = ['inactive', 'recording', 'paused'];
+            expect(recState).toContain(media.state);
+            expect(media.onstart).toBeDefined();
+            expect(media.onstop).toBeDefined();
+            expect(media.ondataavailable).toBeDefined();
+            expect(media.onpause).toBeDefined();
+            expect(media.onresume).toBeDefined();
+            expect(media.onerror).toBeDefined();
+            expect(media.videoBitsPerSecond).toBeDefined();
+            expect(typeof media.videoBitsPerSecond).toBe('number');
+            expect(media.audioBitsPerSecond).toBeDefined();
+            expect(typeof media.audioBitsPerSecond).toBe('number');
+            expect(media.start).toBeDefined();
+            expect(typeof media.start).toBe('function');
+            expect(media.stop).toBeDefined();
+            expect(typeof media.stop).toBe('function');
+            expect(media.pause).toBeDefined();
+            expect(typeof media.pause).toBe('function');
+            expect(media.resume).toBeDefined();
+            expect(typeof media.resume).toBe('function');
+            expect(media.requestData).toBeDefined();
+            expect(typeof media.requestData).toBe('function');
+            expect(media.isTypeSupported).toBeDefined();
+            expect(typeof media.isTypeSupported).toBe('function');
+            expect(media.isTypeSupported('video/mp4')).toEqual(false);
+            expect(media.isTypeSupported('audio/m4a')).toEqual(true);
+            expect(media.isTypeSupported('audio/wav')).toEqual(true);
+            expect(media.isTypeSupported('video/quicktime')).toEqual(true);
+            expect(media.isTypeSupported('')).toEqual(true);
+            media.start();
+            expect(media.state).toBe('recording');
+            media.stop();
+            expect(media.state).toBe('inactive');
+        });
+
+        it('should take a MediaStream as constructor argument with video only', function () {
+            var options = {mimeType : 'video/quicktime'};
+            var media = new MediaRecorder(stream3);
+            expect(media).toBeDefined();
+            expect(typeof media === 'object').toBe(true);
+            expect(media.stream).toBeDefined();
+            expect(media.mimeType).toBeDefined();
+            expect(typeof media.mimeType).toBe('string');
+            expect(media.state).toBeDefined();
+            var recState = ['inactive', 'recording', 'paused'];
+            expect(recState).toContain(media.state);
+            expect(media.onstart).toBeDefined();
+            expect(media.onstop).toBeDefined();
+            expect(media.ondataavailable).toBeDefined();
+            expect(media.onpause).toBeDefined();
+            expect(media.onresume).toBeDefined();
+            expect(media.onerror).toBeDefined();
+            expect(media.videoBitsPerSecond).toBeDefined();
+            expect(typeof media.videoBitsPerSecond).toBe('number');
+            expect(media.audioBitsPerSecond).toBeDefined();
+            expect(typeof media.audioBitsPerSecond).toBe('number');
+            expect(media.start).toBeDefined();
+            expect(typeof media.start).toBe('function');
+            expect(media.stop).toBeDefined();
+            expect(typeof media.stop).toBe('function');
+            expect(media.pause).toBeDefined();
+            expect(typeof media.pause).toBe('function');
+            expect(media.resume).toBeDefined();
+            expect(typeof media.resume).toBe('function');
+            expect(media.requestData).toBeDefined();
+            expect(typeof media.requestData).toBe('function');
+            expect(media.isTypeSupported).toBeDefined();
+            expect(typeof media.isTypeSupported).toBe('function');
+            expect(media.isTypeSupported('video/mp4')).toEqual(false);
+            expect(media.isTypeSupported('audio/m4a')).toEqual(true);
+            expect(media.isTypeSupported('audio/wav')).toEqual(true);
+            expect(media.isTypeSupported('video/quicktime')).toEqual(true);
+            expect(media.isTypeSupported('')).toEqual(true);
+            media.start();
+            expect(media.state).toBe('recording');
+            media.stop();
+            expect(media.state).toBe('inactive');
+        });
     });
 });
